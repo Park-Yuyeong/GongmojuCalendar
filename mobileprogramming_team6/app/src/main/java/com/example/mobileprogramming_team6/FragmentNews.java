87b18b6ccab5c52b20c9db16fedb1f387158fc2c
22sonamu <22sonamu@naver.com>
2021-11-17 01:34:13 +0900

    bottomnavigationview

---
+++ mobileprogramming_team6/app/src/main/java/com/example/mobileprogramming_team6/FragmentNews.java
@@ -0,0 +1,16 @@
+package com.example.mobileprogramming_team6;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentNews extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_news, container, false);
+    }
+}
