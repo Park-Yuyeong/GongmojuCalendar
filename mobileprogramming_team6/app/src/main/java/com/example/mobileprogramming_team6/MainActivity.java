9c4587aaf2ecd6457e49c269a2c3aee0aa8f036b
Park-Yuyeong <dbduda1@kookmin.ac.kr>
2021-11-28 18:30:58 +0900

    상태 유지 해제

--- mobileprogramming_team6/app/src/main/java/com/example/mobileprogramming_team6/MainActivity.java
+++ mobileprogramming_team6/app/src/main/java/com/example/mobileprogramming_team6/MainActivity.java
@@ -41,14 +41,14 @@ public class MainActivity extends AppCompatActivity {
     //프래그먼트 선언//
 
     private FragmentManager fragmentManager = getSupportFragmentManager();
-    private Fragment fragmentNews;
-    private Fragment fragmentCalender;
-    private Fragment fragmentCart;
-    private Fragment fragmentCalender_new;
-    //private FragmentNews fragmentNews = new FragmentNews();
-    //private FragmentCalender fragmentCalender = new FragmentCalender();
-    //private FragmentCart fragmentCart = new FragmentCart();
-    //private FragmentCalender_new fragmentCalender_new = new FragmentCalender_new();
+//    private Fragment fragmentNews;
+//    private Fragment fragmentCalender;
+//    private Fragment fragmentCart;
+//    private Fragment fragmentCalender_new;
+    private FragmentNews fragmentNews;
+    private FragmentCalender fragmentCalender;
+    private FragmentCart fragmentCart;
+    private FragmentCalender_new fragmentCalender_new;
 
 
 
@@ -61,8 +61,8 @@ public class MainActivity extends AppCompatActivity {
 
         fragmentNews = new FragmentNews();
 
-        transaction.replace(R.id.frameLayout, fragmentNews).commit();
-        //transaction.replace(R.id.frameLayout, fragmentNews).commitAllowingStateLoss();
+        //transaction.replace(R.id.frameLayout, fragmentNews).commit();
+        transaction.replace(R.id.frameLayout, fragmentNews).commitAllowingStateLoss();
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
 
@@ -94,37 +94,46 @@ public class MainActivity extends AppCompatActivity {
             FragmentTransaction transaction = fragmentManager.beginTransaction();
             switch(item.getItemId()){
                 case R.id.newsItem:
-                    //transaction.replace(R.id.frameLayout, fragmentNews).commitAllowingStateLoss();
-                    if (fragmentNews == null){
+                    //if (fragmentCalender_new != null)   transaction.hide(fragmentCalender_new).commitAllowingStateLoss();
+//                    transaction.replace(R.id.frameLayout, fragmentNews).commitAllowingStateLoss();
+                    //if (fragmentNews == null){
                         fragmentNews = new FragmentNews();
-                        fragmentManager.beginTransaction().add(R.id.frameLayout, fragmentNews).commit();
-                    }
-
-                    if (fragmentNews != null)   fragmentManager.beginTransaction().show(fragmentNews).commit();
-                    if (fragmentCalender_new != null)   fragmentManager.beginTransaction().hide(fragmentCalender_new).commit();
-                    if (fragmentCart != null)   fragmentManager.beginTransaction().hide(fragmentCart).commit();
+                        transaction.add(R.id.frameLayout, fragmentNews).commitAllowingStateLoss();
+                        //fragmentManager.beginTransaction().add(R.id.frameLayout, fragmentNews).commit();
+                    //}
+//
+//                    if (fragmentNews != null)   fragmentManager.beginTransaction().show(fragmentNews).commit();
+                    //if (fragmentCalender_new != null)   transaction.detach(fragmentCalender_new).commitAllowingStateLoss();
+//                    if (fragmentCart != null)   fragmentManager.beginTransaction().hide(fragmentCart).commit();
                     break;
                 case R.id.calenderItem:
                     //transaction.replace(R.id.frameLayout, fragmentCalender_new).commitAllowingStateLoss();
-                    if (fragmentCalender_new == null){
-                        fragmentCalender_new = new FragmentCalender_new();
-                        fragmentManager.beginTransaction().add(R.id.frameLayout, fragmentCalender_new).commit();
-                    }
+                    //transaction.add(R.id.frameLayout, fragmentCalender_new).commitAllowingStateLoss();
 
-                    if (fragmentNews != null)   fragmentManager.beginTransaction().hide(fragmentNews).commit();
-                    if (fragmentCalender_new != null)   fragmentManager.beginTransaction().show(fragmentCalender_new).commit();
-                    if (fragmentCart != null)   fragmentManager.beginTransaction().hide(fragmentCart).commit();
+//                    if (fragmentNews != null) transaction.remove(fragmentNews).commitAllowingStateLoss();
+//                    if (fragmentCart != null) transaction.remove(fragmentCart).commitAllowingStateLoss();
+                    //if (fragmentCalender_new == null){
+                        fragmentCalender_new = new FragmentCalender_new();
+                        //transaction.replace(R.id.frameLayout, fragmentCalender_new).commitAllowingStateLoss();
+                        transaction.add(R.id.frameLayout, fragmentCalender_new).commitAllowingStateLoss();
+                    //}
+
+                    //if (fragmentNews != null)   fragmentManager.beginTransaction().hide(fragmentNews).commit();
+                    //else transaction.show(fragmentCalender_new).commitAllowingStateLoss();
+                    //if (fragmentCart != null)   fragmentManager.beginTransaction().hide(fragmentCart).commit();
                     break;
                 case R.id.cartItem:
+                    //if (fragmentCalender_new != null)   transaction.hide(fragmentCalender_new).commitAllowingStateLoss();
                     //transaction.replace(R.id.frameLayout, fragmentCart).commitAllowingStateLoss();
-                    if (fragmentCart == null){
+                    //if (fragmentCart == null){
                         fragmentCart = new FragmentCart();
-                        fragmentManager.beginTransaction().add(R.id.frameLayout, fragmentCart).commit();
-                    }
-
-                    if (fragmentNews != null)   fragmentManager.beginTransaction().hide(fragmentNews).commit();
-                    if (fragmentCalender_new != null)   fragmentManager.beginTransaction().hide(fragmentCalender_new).commit();
-                    if (fragmentCart != null)   fragmentManager.beginTransaction().show(fragmentCart).commit();
+                        transaction.add(R.id.frameLayout, fragmentCart).commitAllowingStateLoss();
+                        //fragmentManager.beginTransaction().add(R.id.frameLayout, fragmentCart).commit();
+                    //}
+//
+//                    if (fragmentNews != null)   fragmentManager.beginTransaction().hide(fragmentNews).commit();
+                    //if (fragmentCalender_new != null)   transaction.detach(fragmentCalender_new).commitAllowingStateLoss();
+//                    if (fragmentCart != null)   fragmentManager.beginTransaction().show(fragmentCart).commit();
                     break;
 
             }
